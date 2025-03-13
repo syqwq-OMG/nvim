f472bd9b2c0ff379eda6249fbeade255e26bcd56
syqwq-OMG <3422403944@qq.com>
2025-03-13 09:50:34 +0800

    init nvim repo

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.api.nvim_set_keymap
+local del = vim.keymap.del
+-- 复用 opt 参数
+local opt = { noremap = true, silent = true }
+
+del("n", "<c-k>")
+map("n", "<C-j>", "4j", opt)
+map("n", "<C-k>", "4k", opt)
+
+map("n", "<C-S-Left>", ":vertical resize -2<CR>", opt)
+map("n", "<C-S-Right>", ":vertical resize +2<CR>", opt)
---
+++ lua/config/options.lua
@@ -0,0 +1,25 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- 搜索大小写不敏感，除非包含大写
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- 边输入边搜索
+vim.o.incsearch = true
+
+-- 自动补全不自动选中
+vim.g.completeopt = "menu,menuone,noselect,noinsert"
+
+-- 补全增强
+vim.o.wildmenu = true
+
+vim.g.python3_host_prog = "/path/to/conda/environment/bin/python"
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.o.wrap = true
