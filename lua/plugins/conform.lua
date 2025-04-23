e2166e3a91cc69f3a5b4be757e82da269fa4b47c
syqwq-ubuntu-wsl <3422403944@qq.com>
2025-04-23 12:50:42 +0800

    ubuntu updates

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,10 +3,29 @@ return {
     opts = function()
         local plugin = require("lazy.core.config").plugins["conform.nvim"]
         local opts = {
+            default_format_opts = {
+                timeout_ms = 3000,
+                async = false, -- not recommended to change
+                quiet = false, -- not recommended to change
+                lsp_format = "fallback", -- not recommended to change
+            },
+
             formatters_by_ft = {
                 cpp = { "clang_format" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                css = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                lua = { "stylua" },
+                fish = { "fish_indent" },
+                sh = { "shfmt" },
             },
             formatters = {
+                injected = { options = { ignore_errors = true } },
                 clang_format = {
                     prepend_args = { "--style={BasedOnStyle: LLVM, IndentWidth: 4, TabWidth: 4}" },
                 },
